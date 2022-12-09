d1806f139324fa8aa7ee9023be9d8da86aa9bbf4
Lok Leoi <lyulelok@gmail.com>
2022-12-08 22:09:27 -0500

    lab10

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // @author Le Lyu lyule@bc.edu
-// @credit Peiyu Zhong
+// lab 10 based on lab 5
 
 // create svg with margin convention
-d3.select(".chart").append("svg");
 const margin = { top: 50, right: 20, bottom: 20, left: 50 };
 
 const width = 650 - margin.left - margin.right,
@@ -20,8 +19,11 @@ let svg = d3
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .attr("role", "graphics-document")
+    .attr("aria-roledescription", "bar chart")
+    .attr("tabindex", 0)
     .append("g")
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // create scales without domains
 let xScale = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1);
@@ -34,9 +36,7 @@ svg
     .attr("class", "x-axis")
     .attr("transform", `translate(0, ${height})`);
 
-svg
-    .append("g")
-    .attr("class", "y-axis");
+svg.append("g").attr("class", "y-axis");
 
 svg
     .append("text")
@@ -49,36 +49,31 @@ svg
 
 // (Later) Define update parameters: measure type, sorting direction
 
-
-
-
-
 // CHART UPDATES ---------------------------------------------------------
 function update(data, type, reverse) {
-    // Change the measure dynamically: 
+    // Change the measure dynamically:
     // d[type] where type is either "stores" or "revenue"
     // Update scale domains
 
-    // sort the data 
+    // sort the data
     if (!reverse) {
         data.sort((a, b) => b[type] - a[type]);
-    }
-    else if (reverse) {
+    } else if (reverse) {
         data.sort((a, b) => a[type] - b[type]);
-
     }
 
     xScale.domain(data.map((d) => d.company));
-    yScale.domain([0, d3.max(data, function (d) {
-        // console.log(d[type]);
-        return d[type];
-    })])
+    yScale.domain([
+        0,
+        d3.max(data, function (d) {
+            return d[type];
+        }),
+    ]);
     // yScale.domain([0, d3.max(data, (d) => d[type])]);
-    const bars = svg
-        .selectAll(".bar")
-        .data(data, d => d.company);
+    const bars = svg.selectAll(".bar").data(data, (d) => d.company);
     // Implement the enter-update-exist sequence
-    bars.enter()
+    bars
+        .enter()
         .append("rect")
         .attr("class", "bar")
         .attr("x", (d) => xScale(d.company))
@@ -87,15 +82,14 @@ function update(data, type, reverse) {
         .merge(bars)
         .transition()
         .duration(1000)
-        .attr("x", d => xScale(d.company))
-        .attr("y", d => yScale(d[type]))
+        .attr("x", (d) => xScale(d.company))
+        .attr("y", (d) => yScale(d[type]))
         .attr("height", function (d) {
             return height - yScale(d[type]);
         })
         .style("fill", "lightblue");
 
-    bars.exit()
-        .remove();
+    bars.exit().remove();
 
     // Manually update the axis title
     // Update axes and axis title
@@ -108,17 +102,36 @@ function update(data, type, reverse) {
     svg.select(".y-axis").call(yAxis);
     if (type === "stores") {
         svg.select(".y-axis-title").text("Stores");
-    }
-    else if (type === "revenue") {
+    } else if (type === "revenue") {
         svg.select(".y-axis-title").text("Billion USD");
     }
+
+    d3.select(".chart")
+        .select("svg")
+        .attr(
+            "aria-label",
+            type === "stores"
+                ? "bar chart showing eight different coffee stores' number worldwide"
+                : "bar chart showing eight different coffee stores' revenue in billions worldwide"
+        );
+
+    d3.selectAll(".bar")
+        .attr("role", "graphics-symbol")
+        .attr("aria-roledescription", "bar element")
+        .attr("aria-label", d => {
+            return type === "stores" ? `${d.company} has ${d.stores} stores worldwide` : `${d.company} has ${d.revenue} billions revenue worldwide`
+        }
+        )
+        .attr("tabindex", 0);
 }
+
 // CHART UPDATES ---------------------------------------------------------
 // Loading data
 // define a global variable d to store data
 let d;
 d3.csv("coffee-house-chains.csv", d3.autoType).then((data) => {
     // create initial bar chart
+    // d is used to stored the data so that it could be passed later.
     d = data;
     update(data, type, reverse);
 });
@@ -131,8 +144,12 @@ d3.select("#group-by").on("change", function () {
     update(d, this.value, reverse);
 });
 
-
-d3.select("#btn").on("click", function() {
+d3.select("#btn").on("click", function () {
     reverse = !reverse;
     update(d, currentSelection, reverse);
-})
+});
+
+
+d3.select(".x-axis").attr("aria-hidden", "true");
+d3.select(".y-axis").attr("aria-hidden", "true");
+d3.select(".y-axis-title").attr("aria-hidden", "true");
